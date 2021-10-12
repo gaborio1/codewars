c0937e3ebe3a16358aaa64812be83f7c767cf377
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-10-12 15:53:56 +0100

    add 5kyu - anagrams() - MacBook

--- kyu5.js
+++ kyu5.js
@@ -1,4 +1,80 @@
 
+// ============================================================================
+// ============================================================================
+
+
+
+
+// ============================================================================
+// ============================================================================
+
+
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+
+// 'abba' & 'abca' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+const anagrams = (word, words) => {
+  const solution = [];
+  const sortLetters = (str) => {  //   SORT ALPHABETICALLY
+    return str.toLowerCase().split("").sort().join("");
+  };
+  for (let i = 0; i < words.length; i++) {  // COMPARE TO EACH ELEMENT
+    if (sortLetters(words[i]) === sortLetters(word)) {
+      solution.push(words[i]);  // ADD TO SOLUTION IF IT MATCHES word
+    }
+  }
+  // console.log(solution);
+  return solution;
+};
+
+// anagrams("abba", ["aabb", "abcd", "bbaa", "dada"]);
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']);
+// anagrams('laser', ['lazing', 'lazy',  'lacer']);
+
+// ===== OTHER CODEWARS SOLUTIONS =====
+
+// String.prototype.sort = function() {
+//   return this.split("").sort().join("");
+// };
+
+// function anagrams(word, words) {
+//   return words.filter(function(x) {
+//       return x.sort() === word.sort();
+//   });
+// }
+
+// let anagrams = (word, words) => words.filter(w => w.split('').sort().join('') === word.split('').sort().join(''));
+
+// function anagrams(word, words) {
+//   return words.filter(w => reorder(w) === reorder(word));
+// }
+// function reorder(word) {
+//   return word.split('').sort().join('');
+// }
+
+// function anagrams(a, b) {
+//   return b.filter(w=>''+[...a].sort()===''+[...w].sort());
+// }
+
+
+
+// ============================================================================
+// ============================================================================
+
+
 // !!!!!!!!!!!!!!!!! CODE TIMES OUT !!!!!!!!!!!!!!!!!!!!!
 
 // Sum of Pairs
