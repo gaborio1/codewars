6c802646465ece232aa748e66560bdb3f819cd96
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-11-06 22:29:04 +0000

    add other solutions to 5kyu sumPairs

--- kyu5.js
+++ kyu5.js
@@ -747,6 +747,27 @@ const sumPairs = (ints, sum) => {
 // sumPairs([10, 5, 2, 3, 7, 5], 10);
 // sumPairs([1, 4, 8, 7, 3, 15], 8);
 
+
+// =================== OTHER CODEWARS SOLUTIONS: =============================
+
+// function sum_pairs(ints, s) {
+//   let seen = new Set();
+//   for (let i of ints) {
+//     if (seen.has(s - i)) return [s - i, i];
+//     seen.add(i);
+//   }
+// }
+
+// WITH OBJECT:
+
+// var sum_pairs=function(ints, s){
+//   var seen = {}
+//   for (var i = 0; i < ints.length; ++i) {
+//     if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+//     seen[ints[i]] = true
+//   }
+// }
+
 // ============================================================================
 // ============================================================================
 
