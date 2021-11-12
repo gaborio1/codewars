257064beff95430d65a385e264e40aab6726f759
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-11-12 13:59:10 +0000

    add 5kyu Pete, the baker - cakes(recipe, available)

--- kyu5.js
+++ kyu5.js
@@ -1,5 +1,58 @@
 
+// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
+// TITLE:  Pete, the baker - cakes(recipe, available)
+// 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
+// SOURCE: 
+// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
 
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+const cakes = (recipe, avail) => {
+  const counter = [];
+  for (ingr in recipe) {                                      //  LOOP THROUGH KEYS IN recipe
+      if (!avail[ingr]) return 0;                             //  if NO SUCH ingr IN AVAILABLE return 0
+      counter.push(Math.floor(avail[ingr] / recipe[ingr]));   //  OTHERWISE PUSH QUANTITY IN ARR
+  }
+  return Math.min(...counter);                                // return SMALLEST QUANTITY AS RESULT
+};
+
+// Examples:
+
+// must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+// must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+//============= OTHER CODEWARS SOLUTIONS: =============
+
+// function cakes(recipe, available) {
+//   return Object.keys(recipe).reduce(function(val, ingredient) {
+//     return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+//   }, Infinity)  
+// }
+
+// const cakes = (needs, has) => Math.min(
+//   ...Object.keys(needs).map(key => Math.floor(has[key] / needs[key] || 0))
+// )
+
+
+
+// ===== for...in =====
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+// The for...in statement iterates over all enumerable properties of an object that are keyed by strings (ignoring ones keyed by Symbols), including inherited enumerable properties.
+
+// const object = { a: 1, b: 2, c: 3 };
+
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+// expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
 
 
 // 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
