e529a680f3a5062d0aa44cc19dfbbfe1deaa480e
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-11-13 18:46:49 +0000

    add 5kyu REGEX PASSWORD VALIDATOR to common problems

--- common-problems.js
+++ common-problems.js
@@ -1,3 +1,54 @@
+// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
+// TITLE:  REGEX PASSWORD VALIDATOR
+// 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
+// SOURCE: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
+
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+
+// Valid passwords will only be alphanumeric characters.
+
+// ===== BASED ON: =====
+// (BY "Dana" Jun 28 '20 at 14:48)
+
+// ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$
+
+// At least 8 characters long;
+// One lowercase, one uppercase, one number and one special character;
+// No whitespaces.
+
+function validate(password) {
+	return /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}/.test(password);
+  }
+  
+  validate("fjd3IR9");
+
+  //============= OTHER CODEWARS SOLUTIONS: =============
+
+  // function validate(password) {
+//   return  /^[A-Za-z0-9]{6,}$/.test(password) &&
+//           /[A-Z]+/           .test(password) &&
+//           /[a-z]+/           .test(password) &&
+//           /[0-9]+/           .test(password) ;
+// }
+
+
+// function validate(password) {
+//   /**
+//   * positive lookahead is used here to ensure that at least one of each character set is available
+//   * you need to allow every character (.*) before your lookahead character set, 
+//   * except you want to ensure the expression starts with i.e. a lowercase character.
+//   * Each lookahead basically says "Is there <anything> and 1 or more of <character set> in the following expression?"
+//   * The speciality here is that lookaheads wont touch the matching group so that you can check for 6 or more
+//   * characters afterwards. The 6 or more characters will simply not match if they don't fulfill every lookahead.
+//   **/
+//   return /^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)[A-Za-z0-9]{6,}$/.test(password);
+// }
 
 // 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
 // TITLE:  Method chaining - x.add(3).substract(2).print()
