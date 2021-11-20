145e20f5cb7ea856a22608b0177aa36973ebbf42
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-11-20 01:52:34 +0000

    add snail - UNSOLVEDgit add .!

--- kyu4.js
+++ kyu4.js
@@ -13,9 +13,108 @@
 // snail(array) #=> [1,2,3,4,5,6,7,8,9]
 // This image will illustrate things more clearly:
 
+// const solution = [];
+
+// const topRow = (arr) => {
+//     console.log("       top row:");
+//     console.log(arr);
+//     if (arr.length === 1) {
+//         solution.push(...arr[arr.length - 1].reverse());
+//         // console.log(arr[arr.length - 1].reverse());
+//         arr.splice(arr.length - 1, 1);
+//         return arr;
+//         //     solution.push(...arr[0]);
+//         //     return arr;
+//     }
+//     solution.push(...arr[0]);
+//     arr.splice(0, 1);
+//     return arr;
+// };
+
+
+
+
+// const rightColumn = (arr) => {
+//     console.log("       right column:");
+//     console.log(arr);
+//     console.log("length: " + arr.length);
+//     if (arr.length === 1) {
+//         console.log("my length is 1, reversed[0]");
+//         // console.log(...arr[0].reverse());
+//         console.log(solution);
+//         solution.push(...arr[0].reverse());
+//         console.log(solution);
+//         // return solution;
+//         arr.splice(0, 1)
+//         return arr;
+//     }
+//     for (elem of arr) {
+//         // console.log(elementArr[elementArr.length - 1]);
+//         // console.log(elem);
+//         solution.push(elem[elem.length - 1]);
+//         elem.splice(elem.length - 1, 1);
+//     }
+//     // console.log(arr);
+//     return arr;
+// };
+
+
+
+
+
+
+// const bottomRow = (arr) => {
+//     console.log("       bottom row:");
+//     if (arr.length === 0) {
+//         console.log("empty array");
+//         return arr;
+//     }
+//     console.log(arr);
+//     if (arr.length === 1) {
+//         solution.push(...arr[0].reverse());
+//         return arr;
+//     } else {
+//         solution.push(...arr[arr.length - 1].reverse());
+//         // console.log(arr[arr.length - 1].reverse());
+//         arr.splice(arr.length - 1, 1);
+//         return arr;
+//     }
+    
+// };
+
+// const leftColumn = (arr) => {
+//     console.log("       left column:");
+//     if (arr.length === 0) {
+//         console.log("empty array");
+//         return arr;
+//     }
+//     console.log(arr);
+//     if (arr.length === 1) {
+//         solution.push(...arr[0]);
+//         arr.splice(0, 1);
+//         return arr;
+//     } else {
+//         // console.log("left: multiple arrays");
+//         // console.log(`array length: ${arr.length}`);
+
+//         let i = arr.length - 1;
+//         for (i; i > -1; i--) {
+//             // console.log(arr[i]);
+//             // console.log(arr[i][0]);
+//             solution.push(arr[i][0]);
+//             arr[i].splice(0, 1);
+//         }
+//         return arr;
+//     }
+// };
+
+
 const solution = [];
 
-const topRow = (arr) => {
+const snail = (array) => {
+    // const solution = [];
+    
+    const topRow = (arr) => {
     console.log("       top row:");
     console.log(arr);
     if (arr.length === 1) {
@@ -84,9 +183,14 @@ const bottomRow = (arr) => {
 
 const leftColumn = (arr) => {
     console.log("       left column:");
+    if (arr.length === 0) {
+        console.log("empty array");
+        return arr;
+    }
     console.log(arr);
     if (arr.length === 1) {
         solution.push(...arr[0]);
+        arr.splice(0, 1);
         return arr;
     } else {
         // console.log("left: multiple arrays");
@@ -102,6 +206,67 @@ const leftColumn = (arr) => {
         return arr;
     }
 };
+
+    
+    if (array.length === 1) {
+        console.log("return array[0]");
+        // return array[0];
+    }
+    topRow(array);
+    rightColumn(array);
+    bottomRow(array);
+    leftColumn(array);
+    
+    // return array.length === 1 ? return array[0] : snail(array);
+    
+     if (array[0] === undefined) {
+        console.log("undefined");
+         // console.log(solution);
+        // return array[0];
+    } else {
+        console.log(solution);
+        return snail(array);
+    }
+    console.log(solution);
+    return solution;
+}
+
+// ===== NOT WORKING: =====
+
+// snail([[1, 2, 3, 4, ], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]);
+
+// snail([
+//     [1, 2, 3, 4, 5],
+//     [16, 17, 18, 19, 6],
+//     [15, 24, 25, 20, 7],
+//     [14, 23, 22, 21, 8],
+//     [13, 12, 11, 10, 9]
+// ]);
+
+// ========== WORKING: ========== (UP TO 3x3)
+
+// snail([[]]);
+
+// snail([[1]]);
+
+// snail([[1, 2],[4, 3]]);
+
+// snail([[1, 2, 3],[8, 9, 4],[7, 6, 5]]);
+
+// ==============================================================================
+
+// const solution = [];
+
+// const reducer = (arr) => {
+//     solution.push(arr[0]);
+//     arr.splice(0, 1);
+//     return arr[0] === undefined ? solution : reducer(arr);
+// }
+
+// reducer([1, 2, 3, 4, 5]);
+
+// ==============================================================================
+
 
 // const snail = (array) => {
 //     if (array.length === 1) {
@@ -145,58 +310,3 @@ const leftColumn = (arr) => {
 //     // console.log("solution:");
 //     return solution;
 // }
-
-const snail = (array) => {
-    // if (array.length === 1) {
-    //     console.log("return array[0]");
-    //     return array[0];
-    // }
-    topRow(array);
-    rightColumn(array);
-    bottomRow(array);
-    leftColumn(array);
-    
-    // return array.length === 1 ? return array[0] : snail(array);
-    
-     if (array.length === 1) {
-        console.log("return array[0]");
-        return array[0];
-    } else {
-        return snail(array);
-    }
-    return solution;
-}
-
-// ===== NOT WORKING: =====
-
-// snail([[1, 2, 3, 4, ], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]);
-
-// snail([
-//     [1, 2, 3, 4, 5],
-//     [16, 17, 18, 19, 6],
-//     [15, 24, 25, 20, 7],
-//     [14, 23, 22, 21, 8],
-//     [13, 12, 11, 10, 9]
-// ]);
-
-// ========== WORKING: ========== (UP TO 3x3)
-
-// snail([[]]);
-
-// snail([[1]]);
-
-// snail([[1, 2],[4, 3]]);
-
-// snail([[1, 2, 3],[8, 9, 4],[7, 6, 5]]);
-
-// ==============================================================================
-
-// const solution = [];
-
-// const reducer = (arr) => {
-//     solution.push(arr[0]);
-//     arr.splice(0, 1);
-//     return arr[0] === undefined ? solution : reducer(arr);
-// }
-
-// reducer([1, 2, 3, 4, 5]);
