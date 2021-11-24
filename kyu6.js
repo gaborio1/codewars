6c019c100602179dda4c2a635efef58e8aaab1c8
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-11-24 23:48:02 +0000

    add new 6kyu's - MacBook

--- kyu6.js
+++ kyu6.js
@@ -52,12 +52,39 @@
 
 
 
-// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
-// TITLE:  
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// TITLE:  TAKE A TEN MINUTE WALK (UNSOLVED)
 // 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
 // SOURCE: 
 // 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
 
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+const isValidWalk = (walk) => {
+    const counter = {};
+    for (dir of walk) {
+        if (!counter[dir]) {
+            counter[dir] = 0;
+        }
+        counter[dir] += 1;
+    }
+    // for (let i = 0; i < walk.length; i++) {
+    //     if (!counter[walk[i]]) {
+    //         counter[walk[i]] = 0;
+    //     }
+    //    counter[walk[i]] += 1;
+    //  }
+    // console.log(counter);
+    return counter["e"] === counter["w"] && counter["n"] === counter["s"];
+};
+
+// isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]);
+// isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']);
+// isValidWalk(["n", "s", "w", "e", "n", "s"]);
+
+!isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])
 
 
 
