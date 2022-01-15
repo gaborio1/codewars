55b015a27573151c02be58b1f11144c6fbcebfaf
gaborio1 <welcome2thepleasuredome@googlemail.com>
2022-01-15 15:29:49 +0000

    add kyu6'S

--- TS/dist/kyu6.js
+++ TS/dist/kyu6.js
@@ -71,13 +71,19 @@
 // SOURCE: 
 // 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
 //============= OTHER CODEWARS SOLUTIONS: =============
-// 🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// TypeError: Reduce of empty array with no initial value
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
 // TITLE:  MULTIPLES OF 3 OR 5
 // 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
 // SOURCE: 
 // 🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰🀰
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// DIVS ARRAY MUST HAVE AN INITIAL VALUE
 const solution = (num) => {
-    const divs = [];
+    const divs = [0];
     for (let i = 1; i < num; i++)
         if (i % 3 === 0 || i % 5 === 0) {
             divs.push(i);
@@ -85,7 +91,16 @@ const solution = (num) => {
     console.log(num >= 0 ? divs.reduce((a, b) => a + b) : 0);
     return num >= 0 ? divs.reduce((a, b) => a + b) : 0;
 };
-solution(10);
-solution(-1);
-solution(0);
+// solution(10);
+// solution(-1);
+// solution(0);
 //============= OTHER CODEWARS SOLUTIONS: =============
+// export class Challenge {
+//     static solution(n: number) {
+//       let x = 0;
+//       for (let i = 0;i < n; ++i)
+//         if (i % 3 == 0 || i % 5 == 0)
+//           x += i
+//       return x;
+//     }
+// }
